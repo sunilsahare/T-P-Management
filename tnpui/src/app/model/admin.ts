85eb5e29841a24423991e8e9949f8b1e6ac94ffe
sunilsahare <61555396+sunilsahare@users.noreply.github.com>
2023-11-28 20:24:45 +0530

    Delete tnpui directory

--- tnpui/src/app/model/admin.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user-model";
-
-export class Admin {
-  userId!: number;
-  user!: User;
-  accessLevel!: string;
-  feedbackAndSupportManagement!: string;
-  systemConfiguration!: string;
-}
--- tnpui/src/app/model/alumni.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user-model";
-
-export class Alumni {
-  userId!: number;
-  user!: User;
-  mentorshipInterests!: string;
-  experienceAndCareerPath!: string;
-  mentorshipHistory!: string;
-}
